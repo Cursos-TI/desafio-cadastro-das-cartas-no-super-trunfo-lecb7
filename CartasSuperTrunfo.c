26b0fe4a0573c727f75999056c6780052bab19c4
lecb7 <lecb79@gmail.com>
2025-06-04 13:59:45 +0000

    final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    
     // Carta 1
     char estado1;
     char cidade1[50];
@@ -81,3 +74,4 @@ int main() {
 
   
     return 0;
+}
